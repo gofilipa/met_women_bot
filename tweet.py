c41d48dbab1277c372ba9a0a3ce8b0dcb4f64028
gofilipa <gofilipa@gmail.com>
2025-03-19 13:31:07 -0400

    fixing indentation damnit

--- tweet.py
+++ tweet.py
@@ -6,13 +6,13 @@ from random import randint
 
 load_dotenv()
 
-consumer = os.environ["consumer"]
+API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
 
 client = tweepy.Client(
-    consumer_key=consumer,
+    consumer_key=API_KEY,
     consumer_secret=API_SECRET,
     access_token=ACCESS_TOKEN,
     access_token_secret=ACCESS_TOKEN_SECRET
