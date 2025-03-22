9be8419cd0f58578f919fc98e5bb2aa0885459df
gofilipa <gofilipa@gmail.com>
2025-03-22 10:02:26 -0400

    removing git from script lol

--- tweet.py
+++ tweet.py
@@ -55,7 +55,7 @@ def tweet_women_fact(tweepy_client):
     if parsed['artistGender'] != '':
         gender = parsed['artistGender']
     else:
-        gender = 'Artist Gender: Unknown'
+        gender = 'Artist gGender: Unknown'
 
     url = parsed['objectURL']
 
@@ -69,7 +69,7 @@ def tweet_women_fact(tweepy_client):
     media = api.media_upload(filename='image.jpg')
     media_id = media.media_id
 
-    tweet_text = f"{title}, {artist}, {gender}, go to artwork: git {url}"
+    tweet_text = f"{title}, {artist}, {gender}, go to artwork: {url}"
     print('tweeting women from the MET...')
 
     tweepy_client.create_tweet(text=tweet_text, media_ids=[media_id])
